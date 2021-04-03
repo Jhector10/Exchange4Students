01e73db694a78f1f58cb80e9b6632a8149d8a5f0
Jhector10 <jashsmash10@gmail.com>
2021-04-03 01:29:33 -0400

    added Authentication page + services module. Added Firebase project in app module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavigationBarComponent
-  ],
+  declarations: [ AppComponent, NavigationBarComponent ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp( {
+      apiKey: "AIzaSyD4sVCwHgz1GUoP7ITGkdg4TtpvDX8uX2o",
+      authDomain: "exchange4students.firebaseapp.com",
+      projectId: "exchange4students",
+      storageBucket: "exchange4students.appspot.com",
+      messagingSenderId: "1081070035803",
+      appId: "1:1081070035803:web:6d9f8bf34a862a1ae1f07e",
+      measurementId: "G-F1NZZJSDQT"
+  }),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
