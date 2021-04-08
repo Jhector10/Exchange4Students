ca2928bc033864fe5c61d1654d7a011cde84b7fe
Mathieu Nagle <62767413+Mathieu-Nagle@users.noreply.github.com>
2021-04-07 23:30:56 -0400

    Added create function

--- src/app/listing-form/listing-form.component.ts
+++ src/app/listing-form/listing-form.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, Validators } from '@angular/forms';
 import {FormBuilder,  FormControl} from '@angular/forms';
 import 'jquery';
 import * as $ from "jquery";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
   selector: 'app-listing-form',
@@ -16,7 +17,7 @@ export class ListingFormComponent {
   //Creating a FormBuilder
   //Creating a FormBuilder
   
-  constructor() {}
+  constructor(private firestore: AngularFirestore) {}
 
   //Grouping the Listing Form under the same attributes
 
@@ -85,8 +86,29 @@ export class ListingFormComponent {
 
   submitted = false;
 
-  onSubmit() { this.submitted = true;}
+  onSubmit() { 
+    this.firestore.collection('books').add({
+      category: this.listingForm.value.category,
+      // title: this.listingForm.get('bookTitle').value,
+      // edition: this.listingForm.value.edition,
+      // courseNum:this.listingForm.value.courseNum,
+      description: this.listingForm.value.description,
+      price: this.listingForm.value.price,
+      itemStatus: this.listingForm.value.itemStatus,
+      exchangeLoc: this.listingForm.value.exchangeLoc,
+      paymentOpt: this.listingForm.value.paymentOpt,
+      // listingPhotos: this.listingForm.value.listingPhotos
 
+  })
+  .then(res => {
+      console.log(res);
+      this.listingForm.reset();
+  })
+  .catch(e => {
+      console.log(e);
+  })
 }
+}
+
 
 
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private firestore: AngularFirestore) {}
+  myArray: any[] = []
   ngOnInit(): void {
+    this.firestore
+  .collection("items")
+  .get()
+  .subscribe((ss) => {
+    ss.docs.forEach((doc) => {
+      this.myArray.push(doc.data());
+    });
+  });
   }
 
 }
