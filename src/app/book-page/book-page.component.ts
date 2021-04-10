75162151f895a3fc8380023217b34634bd4a30f4
LDomadia <leena.domadia2018@gmail.com>
2021-04-10 12:41:05 -0400

    basic listing display

--- src/app/book-page/book-page.component.ts
+++ src/app/book-page/book-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from "@angular/fire/firestore";
 import {AngularFireStorage} from '@angular/fire/storage';
 import firebase from 'firebase/app';
+//import { url } from 'node:inspector';
 
 @Component({
   selector: 'app-book-page',
@@ -33,7 +34,7 @@ export class BookPageComponent implements OnInit {
     const storage = firebase.storage();
     //storage.ref("images0.08732651600669228.png").getDownloadURL()
     const link = storage.ref('SZp6Rcq66OhqWeUOC1tEvmhZbLz2/0.2811381234052286.jpeg').getDownloadURL()
-    .then((url) => {
+    .then((url: string) => {
       console.log(url);
       return url;
     });
@@ -41,7 +42,7 @@ export class BookPageComponent implements OnInit {
     return link;
     */
 
-    
+    /*
     let bucket: any = 'exchange4students.appspot.com';
     let uidtest: any = 'SZp6Rcq66OhqWeUOC1tEvmhZbLz2';
     let listingPhotostest: any = '0.2811381234052286';
@@ -50,8 +51,19 @@ export class BookPageComponent implements OnInit {
     const createPersistentDownloadUrl = (bucket: any, pathToFile: any, downloadToken: any) => {
       return ('https://firebasestorage.googleapis.com/v0/b/'+bucket+'/o/'+uidtest+'%2F'+listingPhotostest+'?alt=media&token='+downloadToken);
     }
+    */
     
     
-    //return 'https://firebasestorage.googleapis.com/v0/b/exchange4students.appspot.com/o/SZp6Rcq66OhqWeUOC1tEvmhZbLz2%2F0.2811381234052286?alt=media&token=3c860626-d6b7-457c-a8bd-fdc02a076c3f';
+    return 'https://firebasestorage.googleapis.com/v0/b/exchange4students.appspot.com/o/SZp6Rcq66OhqWeUOC1tEvmhZbLz2%2F0.2811381234052286?alt=media&token=3c860626-d6b7-457c-a8bd-fdc02a076c3f';
+    
+   /*
+    var url = this.afStorage.ref("SZp6Rcq66OhqWeUOC1tEvmhZbLz2/0.2811381234052286").getDownloadURL()
+    .subscribe((url: string) =>{
+      return url;
+    });
+    */
+    //console.log(url);
+    //return url;
+
   }
 }
