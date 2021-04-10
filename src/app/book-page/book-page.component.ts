997f8a6055b21f578684eba82e3dd232ec009862
Mathieu Nagle <62767413+Mathieu-Nagle@users.noreply.github.com>
2021-04-09 20:26:58 -0400

    Display firestorage data

--- src/app/book-page/book-page.component.ts
+++ src/app/book-page/book-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from "@angular/fire/firestore";
+import {AngularFireStorage} from '@angular/fire/storage';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-book-page',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookPageComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private firestore: AngularFirestore, private afStorage: AngularFireStorage) { }
+  myArray: any[] = []
   ngOnInit(): void {
+    this.firestore
+  .collection("books")
+  .get()
+  .subscribe((ss) => {
+    ss.docs.forEach((doc) => {
+      this.myArray.push(doc.data());
+    });
+  });
+  }
+  getPhoto(uid:string, listingPhoto:string){
+    //This doesn't work yet!!
+    // console.log(firebase.storage().ref('/'+uid+'/'+listingPhoto));
+    // return firebase.storage().ref.('/'+uid+'/'+listingPhoto+'.jpeg').getDownloadURL();
+    // return document.getElementById(uid+'/'+listingPhoto);
+    const storage = firebase.storage();
+    storage.ref('/'+uid+'/'+listingPhoto+'.jpeg').getDownloadURL()
+    .then((url) => {
+      console.log(url);
+      return url;
+    })
   }
 
 }
