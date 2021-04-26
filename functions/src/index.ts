e9c7f18c5a052d2fdfd02ead77ed32d560012e35
Jhector10 <jashsmash10@gmail.com>
2021-04-26 17:58:08 -0400

    still working on email

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,19 +7,21 @@ const db = admin.firestore();
 // Sendgrid Config
 import * as sgMail from "@sendgrid/mail";
 
-const API_KEY = functions.config().sendgrid.key;
-const TEMPLATE_ID = functions.config().sendgrid.template;
+const API_KEY = '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';
+const TEMPLATE_ID = 'd-ce28c69241a04666886c56f0ac374f9d';
 sgMail.setApiKey(API_KEY);
 
 // Emails the buyer when a new order is placed
-export const orderEmail = functions.firestore.document(`orders/{ordersId}`).onCreate( async (change, context) => {
+export const orderEmail = functions.firestore.document(`orders/{ordersId}`).onCreate( async (snap, context) => {
 
     // Read the post document
     const userSnap = await db.collection('user').doc(context.params.userId).get();
 
     // Raw Data
     const user = userSnap.data();
-    const orders = change.data();
+    const orders = snap.data();
+
+    console.log('Running function correctly')
 
     // Email
     const msg = {
