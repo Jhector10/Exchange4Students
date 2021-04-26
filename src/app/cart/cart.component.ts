a784a6288f3c1b5fd57b8728a344db6619e3e31f
LDomadia <leena.domadia2018@gmail.com>
2021-04-26 13:53:42 -0400

    added place order button

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from "@angular/fire/firestore";
 import { AuthService } from '../services/auth.service';
 import { CartService } from '../services/cart.service';
+import { MailchimpService } from '../services/mailchimp.service';
 import { AngularFireFunctions } from '@angular/fire/functions';
 import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase/app';
@@ -17,6 +18,7 @@ export class CartComponent implements OnInit {
     private firestore: AngularFirestore, 
     private authService: AuthService, 
     public cart: CartService,
+    public mailchimp: MailchimpService,
     public afAuth: AngularFireAuth, 
     private fun: AngularFireFunctions) { }
 
