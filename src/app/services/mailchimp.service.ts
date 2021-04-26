f926245ea04d8b8e68e4ef2b60b9fd791e892211
LDomadia <leena.domadia2018@gmail.com>
2021-04-26 13:58:32 -0400

    added correct mandrill api key

--- src/app/services/mailchimp.service.ts
+++ src/app/services/mailchimp.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 declare function require(name:string): any;
-const mailchimpTx = require("@mailchimp/mailchimp_transactional")("3b60730a93b9dc322837e60ae24b887d-us1");
+const mailchimpTx = require("@mailchimp/mailchimp_transactional")("bqdLn-ZGdNXorUrNjVlKnA");
 
 @Injectable({
   providedIn: 'root'
